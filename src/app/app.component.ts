121c5a5352ae77d9acba42b2dd64f1339c8a987e
YuBanMena <yubanmenazabala@outlook.com>
2018-12-11 23:42:00 -0600

    Manejo de Datos e Interpolación
    Descubrí el concepto de interpolación a la forma de imprimir valores del ts a la vista mediante {{  }}

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularProyect01';
+  title = 'YuBan Proyect';
+  nombre = 'Yu Ban Mena Zabala';
+  edad = 22;
+  email = 'yubanmenazabala@outlook.com';
+  frameworks = ['laravel', 'grails', 'angular', 'react'];
+
+  materias = [7500, 1200, 543, 1234];
+  activo = true;
+
+  esActivo() {
+    if (this.activo)
+      return 'Trabajador Activo';
+    else
+      return 'Trabajador Inactivo'
+  }
+
+  ultimos3Sueldos() {
+    let suma = 0;
+    for (let x = 0; x < this.materias.length; x++)
+      suma += this.materias[x];
+    return suma;
+  }
+
 }
